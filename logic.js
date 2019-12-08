eeedf6939317d6599aa53be3dfddd235230b0acd
unknown <mattuy001@gmail.com>
2019-12-08 21:24:35 +1300

    added opposite

--- logic.js
+++ logic.js
@@ -1,17 +1,3 @@
-var input = null;
-var operator = "";
-var num1 = "";
-var num2 = "";
-var total = 0;
-var initialize = false;
-var reset = false;
-var h = [];
-var showTotal = document.getElementById("display");
-var showHistory = document.getElementById("history");
-var showOperator = document.getElementById("operator");
-var checkDigit = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
-var checkOperators = ["+", "-", "*", "/"];
-
 document.onkeydown = function(e) {
   if (checkDigit.includes(e.key)) {
     setValue(e.key);
@@ -23,27 +9,13 @@ document.onkeydown = function(e) {
   if (e.key === "NumpadEnter" || e.key === "Enter") equal();
 };
 
-var math_it_up = {
-  "+": function(x, y) {
-    return x + y;
-  },
-  "-": function(x, y) {
-    return x - y;
-  },
-  "*": function(x, y) {
-    return x * y;
-  },
-  "/": function(x, y) {
-    return x / y;
-  }
-};
-
 function setValue(n) {
   if (input === "0" && n === "0") {
     return (display.innerText = 0);
   }
   if (input === null) input = "";
   if (input.includes(".") && n === ".") return;
+  if (n === ".") input = "0";
   if (reset) {
     input = "";
     reset = false;
@@ -68,6 +40,12 @@ function setOperator(o) {
   operator = o;
   showHistory.innerText = h.join(" ");
   showOperator.innerText = " " + operator;
+}
+
+function opposite() {
+  if (input === null) return;
+  input = String(Number(input) * -1);
+  showTotal.innerText = input;
 }
 
 function equal() {
--- variables.js
+++ variables.js
@@ -1,26 +1,27 @@
-var checkDigit = [
-  48,
-  48,
-  49,
-  50,
-  51,
-  52,
-  53,
-  54,
-  55,
-  56,
-  57,
-  58,
-  96,
-  97,
-  98,
-  99,
-  100,
-  101,
-  102,
-  103,
-  104,
-  105,
-  110,
-  190
-];
+var input = null;
+var operator = "";
+var num1 = "";
+var num2 = "";
+var total = 0;
+var initialize = false;
+var reset = false;
+var h = [];
+var showTotal = document.getElementById("display");
+var showHistory = document.getElementById("history");
+var showOperator = document.getElementById("operator");
+var checkDigit = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+var checkOperators = ["+", "-", "*", "/"];
+var math_it_up = {
+  "+": function(x, y) {
+    return x + y;
+  },
+  "-": function(x, y) {
+    return x - y;
+  },
+  "*": function(x, y) {
+    return x * y;
+  },
+  "/": function(x, y) {
+    return x / y;
+  }
+};
